ee30f6b80a80211c616f1859837a394ba3e9e07f
matsu-oficial <esports.matsu@gmail.com>
2026-09-22 19:25:13 -0300

    add: telas, navigation, repository e model

---
+++ app/src/main/java/matsu_oficial/com/github/todolist/navigation/AppNavigation.kt
@@ -0,0 +1,32 @@
+package matsu_oficial.com.github.todolist.navigation
+
+import matsu_oficial.com.github.todolist.ui.FormularioTarefaScreen
+import matsu_oficial.com.github.todolist.ui.ListaTarefasScreen
+import matsu_oficial.com.github.todolist.viewmodel.TarefaViewModel
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation(viewModel: TarefaViewModel) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "lista") {
+        composable("lista") {
+            ListaTarefasScreen(
+                viewModel = viewModel,
+                onNovaTarefa = { navController.navigate("formulario/0") },
+                onEditarTarefa = { id -> navController.navigate("formulario/$id") }
+            )
+        }
+        composable("formulario/{tarefaId}") { backStackEntry ->
+            val tarefaId = backStackEntry.arguments?.getString("tarefaId")?.toInt() ?: 0
+            FormularioTarefaScreen(
+                viewModel = viewModel,
+                tarefaId = tarefaId,
+                onVoltar = { navController.popBackStack() }
+            )
+        }
+    }
+}
--- app/src/main/java/matsu_oficial/com/github/todolist/ui/theme/Theme.kt
+++ app/src/main/java/matsu_oficial/com/github/todolist/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package matsu_oficial.com.github.todolist.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
